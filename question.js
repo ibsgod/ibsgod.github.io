dfebafe63e4cabf5d6d4fec30b096479764e634e
ibsgod <sankeethdude@gmail.com>
2024-08-06 13:47:08 -0400

    idk

--- question.js
+++ question.js
@@ -2,11 +2,17 @@ class Question extends Entity {
   constructor(x, y, w, h, section) {
     super(x, y, w, h, "question", "question.png")
     this.section = section
+    this.animating = false
+    this.origy = y
   }
 
   tick() {
     if (this.collided["mario"] == "down") {
       cock(this.section)
+      this.animating = true
+    }
+    if (this.animating) {
+
     }
   }
 }
